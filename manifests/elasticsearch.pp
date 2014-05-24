5fdfa3e8c227038efa3a7e2e5b4c64d522b510c9
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-05-24 16:07:58 +0200

    Refactoring elasticsearch

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -2,11 +2,6 @@ include role::base
 
 class { 'ssl_certificates': stage => 'setup_repo' }
 
-sudo::config::user { 'admin_group' :
-  user          => '%admin',
-  configuration => 'ALL=(ALL)       NOPASSWD: ALL'
-}
-
 Yum::Repo <| title == 'cegeka-unsigned' |>
 Yum::Repo <| title == 'cegeka-unsigned-i386' |>
 Yum::Repo <| title == 'cegeka-custom' |>
@@ -15,11 +10,22 @@ Yum::Repo <| title == 'puppet' |>
 Yum::Repo <| title == 'puppet-dependencies' |>
 Yum::Repo <| title == 'elasticsearch-1_1' |>
 
+sudo::config::user { 'admin_group' :
+  user          => '%admin',
+  configuration => 'ALL=(ALL)       NOPASSWD: ALL'
+}
+
+File <| title == '/data/logs' |>
+
 sunjdk::instance { 'jdk.1.7.0_06-fcs':
   ensure => 'present',
   jdk_version => '1.7.0_06-fcs'
 }
 
-class { 'elasticsearch': }
+class { 'elasticsearch':
+  version      => '1.1.1-1',
+  versionlock  => true,
+  cluster_name => 'renta'
+}
 
 Sunjdk::Instance['jdk.1.7.0_06-fcs'] -> Class['elasticsearch']
